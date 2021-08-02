a7a98530f018191547a785898911e6a72d62f19e
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-08-02 20:39:12 +0700

    Some changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import tkinter
 
 
+FONT = ("Arial", 20, "normal")
+
+
 def calculate():
-    miles = int(miles_label.get())
-    km = round(miles * 1.609)
-    km_label.config(text=km)
+    miles = float(miles_input.get())
+    km = round(miles * 1.609, 2)
+    km_output.config(text=km)
 
 
 window = tkinter.Tk()
@@ -13,25 +16,23 @@ window.maxsize(500, 400)
 window.title("Mile to Km Converter")
 window.config(padx=20, pady=20)
 
-label_empty = tkinter.Label()
-label_empty.grid(column=0, row=0, padx=10, pady=10)
 
-label = tkinter.Label(text="is equal to", font=("Arial", 20, "normal"))
+label = tkinter.Label(text="is equal to", font=FONT)
 label.grid(column=0, row=1, padx=0, pady=10)
 
-km_label = tkinter.Label(text=0, font=("Arial", 20, "normal"))
-km_label.grid(column=1, row=1, padx=30, pady=10)
+km_output = tkinter.Label(text=0, font=FONT)
+km_output.grid(column=1, row=1, padx=30, pady=10)
 
-miles_label = tkinter.Entry(text=0, font=("Arial", 20, "normal"), width=10)
-miles_label.grid(column=1, row=0, padx=30, pady=10)
+miles_input = tkinter.Entry(text=0, font=FONT, width=10)
+miles_input.grid(column=1, row=0, padx=30, pady=10)
 
-button = tkinter.Button(text="Calculate", font=("Arial", 20, "normal"), width=10, command=calculate)
+button = tkinter.Button(text="Calculate", font=FONT, width=10, command=calculate)
 button.grid(column=1, row=2, padx=30, pady=10)
 
-text_miles_label = tkinter.Label(text="Miles", font=("Arial", 20, "normal"))
+text_miles_label = tkinter.Label(text="Miles", font=FONT)
 text_miles_label.grid(column=2, row=0, padx=30, pady=10)
 
-text_km_label = tkinter.Label(text="Km", font=("Arial", 20, "normal"))
+text_km_label = tkinter.Label(text="Km", font=FONT)
 text_km_label.grid(column=2, row=1, padx=30, pady=10)
 
 window.mainloop()
